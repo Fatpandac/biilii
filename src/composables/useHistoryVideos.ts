2dadf0b3aef3a0f14ffc8c58ef4cc1d69c1b1268
Fatpandac <i@fatpandac.com>
2023-06-21 23:27:17 +0800

    fix: Add check for end of history videos in load function of useHistory

--- src/composables/useHistoryVideos.ts
+++ src/composables/useHistoryVideos.ts
@@ -14,6 +14,10 @@ export function useHistoryVideos() {
   async function load(idx: number) {
     const res = await getHistoryVideos(idx)
 
+    // is End
+    if (!res.data)
+      return
+
     videos.value = [...videos.value, ...res.data.filter(item => item.bvid)]
   }
 

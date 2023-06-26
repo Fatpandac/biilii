77662122ba35cb671d77eb73c5ce574a9cff858c
Fatpandac <i@fatpandac.com>
2023-06-26 22:18:24 +0800

    fix: Resolve Issue with Infinite Scroll and Safari's Popover
    This commit fixes two issues. Firstly, it resolves an issue with infinite-scroll
    in which the infinite-scroll-immediate attribute was being set to a string value
    instead of a boolean. Secondly, it adds a "name" attribute to the Transition
    element in the UserAvatar.vue component for Safari compatibility.
    The name value is set to "nomean".

--- src/components/VideosBoard.vue
+++ src/components/VideosBoard.vue
@@ -10,12 +10,12 @@ defineProps<{
 <template>
   <div
     v-infinite-scroll="callback" infinite-scroll-distance="300"
-    infinite-scroll-immediate="false"
+    :infinite-scroll-immediate="false"
     class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 pb16"
   >
     <VideoCard
       v-for="video in videos" :id="video.id ?? video.aid"
-      :key="video.id" :view="video.stat.view" :pubdate="video.pubdate"
+      :key="video.id ?? video.aid" :view="video.stat.view" :pubdate="video.pubdate"
       :pic="video.pic" :owner-face="video.owner.face" :owner-name="video.owner.name"
       :title="video.title"
     />
--- src/components/molecules/UserAvatar.vue
+++ src/components/molecules/UserAvatar.vue
@@ -9,7 +9,9 @@ const { userInfo } = storeToRefs(userStore())
         <Avatar :avatar-url="userInfo.face ?? ''" />
       </PopoverButton>
       <PopoverOverlay v-if="!userInfo.isLogin" class="fixed inset-0 z-10 bg-black opacity-30" />
+      <!-- Add a name attribute for Safari to load the web page without issues -->
       <Transition
+        name="nomean"
         enter-active-class="transition duration-200 ease-out" enter-from-class="translate-y-1 opacity-0"
         enter-to-class="translate-y-0 opacity-100" leave-active-class="transition duration-150 ease-in"
         leave-from-class="translate-y-0 opacity-100" leave-to-class="translate-y-1 opacity-0"

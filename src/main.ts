1120572d613061f63cf1c5cffcc84f9af699943c
Fatpandac <i@fatpandac.com>
2023-07-04 22:18:00 +0800

    chore: add google analytics

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,15 @@ import './style.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import VueGtag from 'vue-gtag'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 const pinia = createPinia()
 
-app.use(pinia).use(router)
+app.use(pinia).use(router).use(VueGtag, {
+  config: { id: 'G-7DWSQ58M55' },
+})
 
 app.mount('#app')

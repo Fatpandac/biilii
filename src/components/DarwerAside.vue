2b2b5be5515f3ece115bfbc8d7301db7daa8efea
Fatpandac <i@fatpandac.com>
2023-10-02 22:56:27 +0800

    fix: DarwerAside overflow

--- src/components/DarwerAside.vue
+++ src/components/DarwerAside.vue
@@ -12,10 +12,20 @@ const { showDarwer } = storeToRefs(navStore())
 </script>
 
 <template>
-  <ElDrawer v-model="showDarwer" direction="ltr" class="w-60!" :with-header="false">
-    <div class="flex items-center el-header">
+  <ElDrawer v-model="showDarwer" direction="ltr" class="w-60! drawer" :with-header="false">
+    <div class="sticky top-0 z-10 flex items-center bg-white el-header">
       <DarwerIcon class="w-10 h-10 cursor-pointer" @click="showDarwer = !showDarwer" />
     </div>
     <Aside unvariable />
   </ElDrawer>
 </template>
+
+<style>
+.el-drawer__body::-webkit-scrollbar {
+  display: none;
+}
+
+.el-scrollbar {
+  overscroll-behavior: contain
+}
+</style>
